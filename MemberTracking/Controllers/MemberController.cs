23fbd4d9d426aadd0a57906351253394204e963b
Ben Stone <benstone@uiowa.edu>
2022-06-21 09:55:47 -0500

    Add member controller with validation

---
+++ MemberTracking/Controllers/MemberController.cs
@@ -0,0 +1,70 @@
+﻿using MemberTracking.Data.Models;
+using MemberTracking.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemberTracking.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MemberController : ControllerBase
+{
+    private readonly IMemberRepository _membersRepository;
+
+    public MemberController(IMemberRepository membersRepository)
+    {
+        _membersRepository = membersRepository;
+    }
+
+    [HttpGet("")]
+    public async Task<ActionResult<List<Member>>> GetAllMembers()
+    {
+        var members = await _membersRepository.GetAllMembers();
+        return Ok(members);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Member>> GetMemberById(int id)
+    {
+        var member = await _membersRepository.GetMemberById(id);
+        return Ok(member);
+    }
+
+    [HttpPost("")]
+    public async Task<ActionResult> CreateMember([FromBody] Member member)
+    {
+        if (!Member.IsValid(member, requireId: false))
+        {
+            return BadRequest("Invalid member.");
+        }
+
+        await _membersRepository.CreateMember(member);
+        await _membersRepository.SaveChanges();
+        return Ok();
+    }
+
+    [HttpPost("{id:int}")]
+    public async Task<ActionResult<bool>> UpdateMember(int id, [FromBody] Member member)
+    {
+        if (!Member.IsValid(member))
+        {
+            return BadRequest("Invalid member.");
+        }
+
+        var result = await _membersRepository.UpdateMember(id, member);
+        await _membersRepository.SaveChanges();
+        return result;
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<bool>> DeleteMember(int id)
+    {
+        if (id <= 0)
+        {
+            return BadRequest("Invalid Member ID provided.");
+        }
+
+        var result = await _membersRepository.DeleteMember(id);
+        await _membersRepository.SaveChanges();
+        return result;
+    }
+}
--- MemberTracking/Data/Models/Member.cs
+++ MemberTracking/Data/Models/Member.cs
@@ -6,4 +6,20 @@ public class Member
     public string FirstName { get; set; } = string.Empty;
     public string MiddleName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+
+    public static bool IsValid(Member member, bool requireId = true)
+    {
+        if (!new[] { member.FirstName, member.LastName, member.MiddleName }
+                .All(x => x.Length < 30))
+        {
+            return false;
+        }
+
+        if (requireId && member.Id <= 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
