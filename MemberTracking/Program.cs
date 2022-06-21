5ab59141d748ff8043d20acc019526792c563454
Ben Stone <benstone@uiowa.edu>
2022-06-21 09:02:00 -0500

    Add initial project structure with database connection

---
+++ MemberTracking/Program.cs
@@ -0,0 +1,28 @@
+using MemberTracking.Data.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MemberDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:MemberDB"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
