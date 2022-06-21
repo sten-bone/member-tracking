3f7a823bd70db87ae5376224544eb55ce5b02f60
Ben Stone <benstone@uiowa.edu>
2022-06-21 16:15:57 -0500

    Add general member form structure

--- MemberTracking/ClientApp/src/app/components/member-form/member-form.component.ts
+++ MemberTracking/ClientApp/src/app/components/member-form/member-form.component.ts
@@ -1,21 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Member } from 'src/app/models/member';
 import { MemberService } from 'src/app/services/member.service';
 
 @Component({
   selector: 'app-member-form',
   templateUrl: './member-form.component.html',
-  styleUrls: ['./member-form.component.css']
+  styleUrls: ['./member-form.component.css'],
 })
 export class MemberFormComponent implements OnInit {
   @Input() member!: Member;
+  @Output() resetForm = new EventEmitter();
 
-  constructor(private memberService: MemberService) { }
+  constructor(private memberService: MemberService) {}
 
   ngOnInit(): void {}
 
   setToDefault(): void {
-    this.member = new Member;
+    this.member = new Member();
+    this.resetForm.emit();
   }
 
+  addMember(): void {
+    console.log(this.member.id, this.member.firstName);
+    this.setToDefault();
+  }
 }
