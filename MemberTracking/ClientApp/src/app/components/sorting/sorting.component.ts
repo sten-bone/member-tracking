95d83a343aaf1d98098e09047cc3e01103e1ec5d
Ben Stone <benstone@uiowa.edu>
2022-06-22 10:38:14 -0500

    Add sorting component skeleton
    Will be used later to implement alphabetical sorting in member list.

---
+++ MemberTracking/ClientApp/src/app/components/sorting/sorting.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sorting',
+  template: `
+    <p>
+      sorting works!
+    </p>
+  `,
+  styleUrls: ['./sorting.component.css']
+})
+export class SortingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
